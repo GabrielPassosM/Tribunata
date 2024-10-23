f82e255674d28d7d0f419fc2a07273fa8e2f9ecd
Gabriel Martins <78399176+GabrielPassosM@users.noreply.github.com>
2024-10-23 00:16:02 +0000

    feat: Dropdown component

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -1,14 +1,25 @@
+import Dropdown from "../Dropdown"
 import TextField from "../TextField"
 import "./Forms.css"
 
 const Forms = () => {
+
+    const categories = [
+        "Artilheiros",
+        "Assistentes",
+        "MVPs",
+        "Cartões amarelos",
+        "Cartões vermelhos"
+    ]
+
     return (
         <section className="forms">
             <form>
-                <h2>Preencha os dados para criar o cadastro da pessoa</h2>
+                <h2>Preencha os dados para cadastrar o jogador na categoria</h2>
+                <Dropdown label="Categoria" itens={categories} />
                 <TextField label="Nome" placeholder="Digite o seu nome"/>
-                <TextField label="Cargo" placeholder="Digite o seu cargo"/>
                 <TextField label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <TextField label="Quantidade" placeholder="Digite a quantidade para a categoria" />
             </form>
         </section>
     )
