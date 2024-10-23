1aa88ba257aed71d207b8d4df581624aefd3c8d2
Gabriel Martins <78399176+GabrielPassosM@users.noreply.github.com>
2024-10-23 01:40:04 +0000

    feat: handling input values and components states

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Button from "../Button"
 import Dropdown from "../Dropdown"
 import TextField from "../TextField"
@@ -13,14 +14,47 @@ const Forms = () => {
         "Cartões vermelhos"
     ]
 
+    const [category, setCategory] = useState(categories[0])
+    const [name, setName] = useState("")
+    const [image, setImage] = useState("")
+    const [quantity, setQuantity] = useState("")
+
+    const onSave = (event) => {
+        event.preventDefault()
+        console.log("Forms submetido: ", category, name, image, quantity)
+    }
+
     return (
         <section className="forms">
-            <form>
+            <form onSubmit={onSave}>
                 <h2>Preencha os dados para cadastrar o jogador na categoria</h2>
-                <Dropdown label="Categoria" itens={categories} />
-                <TextField label="Nome" placeholder="Digite o seu nome"/>
-                <TextField label="Imagem" placeholder="Digite o endereço da imagem"/>
-                <TextField label="Quantidade" placeholder="Digite a quantidade para a categoria" />
+                <Dropdown
+                    required={true}
+                    label="Categoria"
+                    itens={categories}
+                    selectValue={category}
+                    setSelectValue={value => setCategory(value)}
+                />
+                <TextField 
+                    required={true}
+                    label="Nome"
+                    placeholder="Digite o nome"
+                    inputValue={name}
+                    setInputValue={value => setName(value)}
+                />
+                <TextField
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem"
+                    inputValue={image}
+                    setInputValue={value => setImage(value)}
+                />
+                <TextField
+                    required={true}
+                    label="Quantidade"
+                    placeholder="Digite a quantidade para a categoria" 
+                    inputValue={quantity}
+                    setInputValue={value => setQuantity(value)}
+                />
                 <Button>
                     Cadastrar
                 </Button>
--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -4,7 +4,7 @@ const TextField = (props) => {
     return (
         <div className="text-field">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>
+            <input value={props.inputValue} onChange={event => props.setInputValue(event.target.value)} required={props.required} placeholder={props.placeholder}/>
         </div>
     )
 }
