8e40fabdb761155b67df282c6b3447a1e2d9583f
Gabriel Martins <78399176+GabrielPassosM@users.noreply.github.com>
2024-10-24 01:23:22 +0000

    feat: Player component

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,9 +1,11 @@
+import Player from "../Player"
 import "./Category.css"
 
 const Category = (props) => {
     return (
         <section className="category" style={{ backgroundColor: props.secondColor }}>
-            <h3 style={{borderColor: props.primaryColor}}>{props.name}</h3>
+            <h3 style={{borderColor: props.primaryColor}} >{props.name}</h3>
+            <Player />
         </section>
     )
 }
---
+++ src/components/Player/index.js
@@ -0,0 +1,18 @@
+import "./Player.css"
+
+const Player = () => {
+    return (
+        <div className="player">
+            <div className="header">
+                <img src="https://github.com/GabrielPassosM.png" alt="foto do jogador" />
+            </div>
+
+            <div className="footer">
+                <h4>Gabriel</h4>
+                <h5>21 gols</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Player;
