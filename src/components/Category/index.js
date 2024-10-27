46237cb74883ca96f37f909acdfcc9defe2b6b38
Gabriel Martins <78399176+GabrielPassosM@users.noreply.github.com>
2024-10-27 23:59:04 +0000

    fix: dont show empty categories

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -2,8 +2,9 @@ import Player from "../Player"
 import "./Category.css"
 
 const Category = (props) => {
+    debugger
     return (
-        <section className="category" style={{ backgroundColor: props.secondColor }}>
+        props.players.length > 0 && <section className="category" style={{ backgroundColor: props.secondColor }}>
             <h3 style={{borderColor: props.primaryColor}} >{props.name}</h3>
             <div className="players">
                 {props.players.map(p => <Player key={p.name} headerColor={props.primaryColor} name={p.name} image={p.image} quantity={p.quantity}/>)}
