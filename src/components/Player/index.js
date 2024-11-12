79564cfb933054af58e3fcf27795a9c4c60b04c5
= <=>
2024-11-11 22:25:53 -0300

    feat: react-icons + trash icon for player deletion

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,9 +1,10 @@
 import "./Player.css"
+import { FaTrash } from "react-icons/fa";
 
 const Player = ({ name, image, quantity, headerColor, onDelete }) => {
     return (
         <div className="player">
-            <div className="delete" onClick={onDelete}>deletar</div>
+            <FaTrash size={25} className="delete" onClick={onDelete} />
             <div className="playerHeader" style={{ backgroundColor: headerColor }}>
                 <img src={image} alt="foto do jogador" />
             </div>
