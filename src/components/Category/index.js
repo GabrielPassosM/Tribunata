54f48fda72de5d388048b5a43bc91357349ad7b4
= <=>
2024-11-12 20:39:42 -0300

    refactor: remove secondColor atribute and just apply different opacity

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,14 +1,14 @@
 import Player from "../Player"
 import "./Category.css"
+import hexToRgba from 'hex-to-rgba';
 
-const Category = ({ players, primaryColor, secondColor, name, onDelete }) => {
-    debugger
+const Category = ({ players, color, name, onDelete }) => {
     return (
-        players.length > 0 && <section className="category" style={{ backgroundColor: secondColor }}>
-            <h3 style={{borderColor: primaryColor}} >{name}</h3>
+        players.length > 0 && <section className="category" style={{ backgroundColor: hexToRgba(color, 0.4) }}>
+            <h3 style={{borderColor: color}} >{name}</h3>
             <div className="players">
                 {players.map(p => {
-                    return <Player key={p.name} headerColor={primaryColor} name={p.name} image={p.image} quantity={p.quantity} onDelete={onDelete}/>
+                    return <Player key={p.name} headerColor={color} name={p.name} image={p.image} quantity={p.quantity} onDelete={onDelete}/>
                 })}
             </div>
         </section>
