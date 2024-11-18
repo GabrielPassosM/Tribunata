7a42f48f495c613574673aa416726b3d8a375f85
gabmartins <gabrielpassosmartins@gmail.com>
2024-11-18 18:10:01 -0300

    feat: show player position

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -3,6 +3,13 @@ import { FaTrash } from "react-icons/fa";
 
 const Player = ({ playerInfo, metricInfo, headerColor, onDelete }) => {
 
+    const positionLabel = {
+        "goalkeeper": "Goleiro",
+        "defender": "Zagueiro",
+        "midfielder": "Meio-campo",
+        "forward": "Atacante"
+    }[playerInfo.position]
+
     return (
         <div className="player">
             <FaTrash size={25} className="delete" onClick={() => onDelete(playerInfo.id)} />
@@ -12,6 +19,7 @@ const Player = ({ playerInfo, metricInfo, headerColor, onDelete }) => {
 
             <div className="playerFooter">
                 <h4>{playerInfo.name}</h4>
+                <h4>{positionLabel}</h4>
                 <h5>{metricInfo[0]} {metricInfo[1]}</h5>
             </div>
         </div>
