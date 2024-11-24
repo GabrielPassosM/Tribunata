3cc512c02bab67e86e1d47e048ea25b02091edef
gabmartins <gabrielpassosmartins@gmail.com>
2024-11-24 17:49:00 -0300

    refactor(apiService): use Axios on fetchPlayers

--- src/apiService.js
+++ src/apiService.js
@@ -1,9 +1,20 @@
+import axios from "axios";
+import { showErrorModal } from "./utils/modalUtils";
+import { unexpectedErrorMsg } from "./consts.js"
+
+
 const apiUrl = process.env.REACT_APP_API_URL;
 
+
 export const fetchPlayers = async () => {
-  const response = await fetch(`${apiUrl}/players`);
-  if (!response.ok) {
-    throw new Error('Failed to fetch players');
+  try {
+    const response = await axios.get(`${apiUrl}/players`)
+    return response.data
+  } catch(error) {
+    let errorMsg = unexpectedErrorMsg
+    if (error.status < 500) {
+      errorMsg = error.response?.data?.detail ?? unexpectedErrorMsg
+    }
+    showErrorModal({text: errorMsg})
   }
-  return response.json();
-};
+}
